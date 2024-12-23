00ebb386df9177171d2d71a444a8dc7daaa16fd8
sssseaurchin <157834865+sssseaurchin@users.noreply.github.com>
2024-12-23 05:37:52 +0300

    Manually updating app.js

--- app.js
+++ app.js
@@ -2,40 +2,46 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const initDatabase = require('./db/init_db');
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-initDatabase().then(() => {
-  console.log('Database initialized successfully.');
+let dbInitialized = false;
 
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug');
+if (!dbInitialized) {
+  initDatabase()
+      .then(() => {
+        console.log('Database initialized successfully.');
+        dbInitialized = true;
+      })
+      .catch((err) => {
+        console.error('Database initialization failed:', err.message);
+        process.exit(1);
+      });
+}
 
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-  app.use('/', indexRouter);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use(function(req, res, next) {
-    next(createError(404));
+app.use('/', indexRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
   });
-
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-
-}).catch((err) => {
-  console.error('Database initialization failed:', err.message);
 });
 
 module.exports = app;
